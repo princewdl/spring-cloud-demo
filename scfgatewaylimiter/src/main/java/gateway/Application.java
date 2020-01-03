b9cd6ede7d48c226096962cb8cf0f00834d4a372
princewdl <924298883@qq.com>
2020-01-03 14:40:11 +0800

    学习提交

--- scfgatewaylimiter/src/main/java/gateway/Application.java
+++ scfgatewaylimiter/src/main/java/gateway/Application.java
@@ -30,4 +30,15 @@ public class Application {
     KeyResolver userKeyResolver() {
         return exchange -> Mono.just(exchange.getRequest().getQueryParams().getFirst("user"));
     }
+
+
+
+
+
+    //用jmeter进行压测，配置10thread去循环请求lcoalhost:8081，循环间隔1s。从压测的结果上看到有部分请求通过，由部分请求失败。
+    // 通过redis客户端去查看redis中存在的key。如下：
+
+    //可见，RequestRateLimiter是使用Redis来进行限流的，并在redis中存储了2个key。关注这两个key含义可以看lua源代码。
+
+
 }
